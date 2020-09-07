3e3d070fd15ffdcfa14628e880ca24cec9ccd7a6
MetLee <met@whu.edu.cn>
2020-09-07 20:02:53 +0800

    modify

--- multipleClassificationPredictionIndex.m
+++ multipleClassificationPredictionIndex.m
@@ -2,12 +2,6 @@ function p = multipleClassificationPredictionIndex(h)
 
     % Compute the prediction (the index of the max element).
 
-    m = size(h, 1);
-    p = zeros(m, 1);
-
-    for i = 1:m
-        [val, ind] = max(h(i, :));
-        p(i) = ind;
-    end
+    [val, p] = max(h, [], 2);
 
 end
