6a84aeaf6386f90f7fa8205f4681dba2f1262d10
MetLee <met@whu.edu.cn>
2020-09-07 17:28:51 +0800

    modify

--- logisticRegressionPrediction.m
+++ logisticRegressionPrediction.m
@@ -6,12 +6,6 @@ function p = logisticRegressionPrediction(X, theta)
     p = zeros(m, 1);
     
     z = X * theta;
-    for i = 1:m
-        if z(i) >= 0 % sigmoid(z) >= 0.5 ==> Z >= 0
-            p(i) = 1;
-        else
-            p(i) = 0;
-        end
-    end
+    p = z >= 0; % sigmoid(z) >= 0.5 ==> Z >= 0
 
 end
