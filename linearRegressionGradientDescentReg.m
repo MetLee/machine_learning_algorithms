689281bf2a5374354683d9ac92791b130a18e15e
MetLee <met@whu.edu.cn>
2020-09-19 22:35:45 +0800

    modify

--- linearRegressionGradientDescentReg.m
+++ linearRegressionGradientDescentReg.m
@@ -3,10 +3,9 @@ function theta = linearRegressionGradientDescentReg(X, y, theta, alpha, lambda, 
     % Compute the modified theta taking num_iters steps' gradient descent with regularization.
 
     m = length(y);
-    n_plus_1 = size(X, 2);
     
-    theta_without_theta0 = theta(2:n_plus_1); % do not regularize theta(1)
-    X_without_X0 = X(:, [2:n_plus_1]);
+    theta_without_theta0 = theta(2:end); % do not regularize theta(1)
+    X_without_X0 = X(:, 2:end);
     for iter = 1:num_iters
         grad0 = 1 / m * ((X * theta - y)' * X)';
         grad_rest = 1 / m * (((h - y)' * X_without_X0)' + lambda * theta_without_theta0);
--- logisticRegressionCostReg.m
+++ logisticRegressionCostReg.m
@@ -3,17 +3,16 @@ function [J, grad] = logisticRegressionCostReg(X, y, theta, lambda)
     % Compute the cost and the gradient of logistic regression algorithm with regularization.
 
     m = length(y);
-    n_plus_1 = size(X, 2);
     
     z = X * theta;
     h = sigmoid(z);
     costMatrix = - y .* log(h) - (1 - y) .* log(1 - h);
-    theta_without_theta0 = theta(2:n_plus_1); % do not regularize theta(1)
+    theta_without_theta0 = theta(2:end); % do not regularize theta(1)
     regMatrix = 1 / 2 * lambda * theta_without_theta0 .^ 2;
     J = 1 / m * (sum(costMatrix) + sum(regMatrix));
 
     grad0 = 1 / m * ((h - y)' * X(:, 1))';
-    X_without_X0 = X(:, [2:n_plus_1]);
+    X_without_X0 = X(:, 2:end);
     grad_rest = 1 / m * (((h - y)' * X_without_X0)' + lambda * theta_without_theta0);
     grad = [grad0; grad_rest];
 
