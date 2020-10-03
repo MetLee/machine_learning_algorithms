f9e885653eedaca76867bab4e83aa5d141c4ce3b
MetLee <met@whu.edu.cn>
2020-10-04 04:54:58 +0800

    Variable threshold.

--- logisticRegressionPrediction.m
+++ logisticRegressionPrediction.m
@@ -1,11 +1,16 @@
-function p = logisticRegressionPrediction(X, theta)
+function p = logisticRegressionPrediction(X, theta, threshold)
 
-    % Compute the prediction using a threshold at 0.5.
+    % Compute the prediction.
+
+    if nargin == 2
+        threshold = 0.5
+    end
 
     m = size(X, 1);
     p = zeros(m, 1);
     
     z = X * theta;
-    p = z >= 0; % sigmoid(z) >= 0.5 ==> Z >= 0
+    h = sigmoid(z);
+    p = h >= threshold;
 
 end
