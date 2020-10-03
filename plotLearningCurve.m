05720aafe2f6d70f1428a6fb2bf0a29c89e3538b
MetLee <met@whu.edu.cn>
2020-10-03 23:07:50 +0800

    Update plotLearningCurve.m

--- plotLearningCurve.m
+++ plotLearningCurve.m
@@ -14,8 +14,8 @@ function rst = plotLearningCurve(X, y, X_cv, y_cv, costFunction, lambda)
     legend('Train', 'Cross Validation');
     xlabel('Number of training examples');
     ylabel('Error');
-    x_axis_max = m + 1;
-    y_axis_max = max(max(error_train), max(error_cv)) + 10;
-    axis([0 x_axis_max 0 y_axis_max]);
+    %x_axis_max = m + 1;
+    %y_axis_max = max(max(error_train), max(error_cv)) + 10;
+    %axis([0 x_axis_max 0 y_axis_max]);
 
 end
